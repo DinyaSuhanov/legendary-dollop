2d901936c7d7ccd1b218b8a32d68fdf75b4040fb
Dinya Suhanov <enkont@mail.ru>
2023-12-27 21:56:15 +0500

    added DZ z1

--- Program.cs
+++ Program.cs
@@ -32,12 +32,54 @@
 
 
 //задание на работу с циклом
-Console.Write("Введи число N: ");
-int N = Convert.ToInt32(Console.ReadLine());            //задаем N
-int negativN = N * -1;                                  //задаем N отрицательное значение
+// Console.Write("Введи число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());            //задаем N
+// int negativN = N * -1;                                  //задаем N отрицательное значение
 
-while (negativN <= N)
+// while (negativN <= N)
+// {
+//     Console.WriteLine(negativN + " ");
+//     negativN++;                                         //negativN = negativN + 1   - инкремент; negativN-- = negativN -1  - декремент
+// }
+
+
+//ДЗ Задача N1
+using System;
+
+public class Answer
 {
-    Console.WriteLine(negativN + " ");
-    negativN++;                                         //negativN = negativN + 1   - инкремент
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        // Введите свое решение ниже
+        if (firstNumber > secondNumber)
+            Console.WriteLine("Первое число "+"`"+ firstNumber + "`"+" больше чем второе число"+  "`" + secondNumber + "`");
+        else        
+            if (firstNumber < secondNumber)
+            Console.WriteLine("Первое число "+"`" + firstNumber + "`"+" меньше чем второе число "+"`" + secondNumber + "`");            
+            else
+                Console.WriteLine("Введенные числа равны "+ "`" +firstNumber+ "`");
+        
+    }
+
+    // Не удаляйте и не меняйте метод Main!
+    static public void Main(string[] args)
+    {
+        int firstNumber,
+            secondNumber;
+
+        if (args.Length >= 2)
+        {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        }
+        else
+        {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 9;
+            secondNumber = 9;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
 }
