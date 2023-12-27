9504bef0a3b0c22199eb05b267a961c17667d5c9
Dinya Suhanov <enkont@mail.ru>
2023-12-27 16:15:59 +0500

    added Konkatenation

--- Program.cs
+++ Program.cs
@@ -2,5 +2,16 @@
 //для многострочного коменнтирования выделяем строки для комментария и нажимаем "контрол + правый слэш"
 //или /*коментарий*/
 
-int number = 9;
-int result = numder * number;
+// int number = 9;
+// int result = numder * number;
+// "привет" + "мир" = "привет мир" - конкатенация
+// Console.ReadLine(); - получить информацию из строчки
+
+
+Console.WriteLine("Введите число");              //  Вывод сообщения 
+
+int input = Convert.ToInt32(Console.ReadLine()); //  int - число, string - строка. convert.toInt32 - конвертация в числовой метод. 
+                                                 //  console.readle - получение информации из введенной строчки
+
+Console.WriteLine(input + 1000);                 //  вывод результата сложения переменной input  + 1000
+
