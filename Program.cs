34b2a6001ca9290c1153dbd96c36f7c20dccf48c
Dinya Suhanov <enkont@mail.ru>
2023-12-27 16:39:54 +0500

    added chek number square

--- Program.cs
+++ Program.cs
@@ -8,10 +8,24 @@
 // Console.ReadLine(); - получить информацию из строчки
 
 
-Console.WriteLine("Введите число");              //  Вывод сообщения 
+// Console.WriteLine("Введите число");              //  Вывод сообщения 
 
-int input = Convert.ToInt32(Console.ReadLine()); //  int - число, string - строка. convert.toInt32 - конвертация в числовой метод. 
-                                                 //  console.readle - получение информации из введенной строчки
+// int input = Convert.ToInt32(Console.ReadLine()); //  int - число, string - строка. convert.toInt32 - конвертация в числовой метод. 
+//                                                  //  console.readle - получение информации из введенной строчки
 
-Console.WriteLine(input + 1000);                 //  вывод результата сложения переменной input  + 1000
+// Console.WriteLine(input + 1000);                 //  вывод результата сложения переменной input  + 1000
 
+Console.Write("Введи первое число: ");                //a
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи второе число: ");                //b
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber == secondNumber * secondNumber)      // "=" - придание значения, "==" - проверка равно/неравно
+{
+    Console.WriteLine("Да, " +firstNumber+ " является квадратом от числа " +secondNumber);
+}
+else                                                // если число а не является квадратом от числа b
+{
+    Console.WriteLine("Нет, " +firstNumber+ " НЕ является квадратом от числа " +secondNumber);
+}
