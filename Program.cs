9857081bdc0030b8e8abd4a661872544d92c6250
Dinya Suhanov <enkont@mail.ru>
2023-12-28 12:08:54 +0500

    added DZ z2

--- Program.cs
+++ Program.cs
@@ -43,43 +43,95 @@
 // }
 
 
-//ДЗ Задача N1
+                                                //ДЗ Задача N1
+                                                
+// using System;
+
+// public class Answer
+// {
+//     static void CompareNumbers(int firstNumber, int secondNumber)
+//     {
+//         // Введите свое решение ниже
+//         if (firstNumber > secondNumber)
+//             Console.WriteLine("Первое число "+"`"+ firstNumber + "`"+" больше чем второе число"+  "`" + secondNumber + "`");
+//         else
+//             if (firstNumber < secondNumber)
+//             Console.WriteLine("Первое число "+"`" + firstNumber + "`"+" меньше чем второе число "+"`" + secondNumber + "`");
+//             else
+//                 Console.WriteLine("Введенные числа равны "+ "`" +firstNumber+ "`");
+
+//     }
+
+//     // Не удаляйте и не меняйте метод Main!
+//     static public void Main(string[] args)
+//     {
+//         int firstNumber,
+//             secondNumber;
+
+//         if (args.Length >= 2)
+//         {
+//             firstNumber = int.Parse(args[0]);
+//             secondNumber = int.Parse(args[1]);
+//         }
+//         else
+//         {
+//             // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             firstNumber = 9;
+//             secondNumber = 9;
+//         }
+
+//         // Не удаляйте строки ниже
+//         CompareNumbers(firstNumber, secondNumber);
+//     }
+// }
+
+
+                                                    //ДЗ задача 2
+
 using System;
 
 public class Answer
 {
-    static void CompareNumbers(int firstNumber, int secondNumber)
+    static int FindMax(int a, int b, int c)
     {
         // Введите свое решение ниже
-        if (firstNumber > secondNumber)
-            Console.WriteLine("Первое число "+"`"+ firstNumber + "`"+" больше чем второе число"+  "`" + secondNumber + "`");
-        else        
-            if (firstNumber < secondNumber)
-            Console.WriteLine("Первое число "+"`" + firstNumber + "`"+" меньше чем второе число "+"`" + secondNumber + "`");            
-            else
-                Console.WriteLine("Введенные числа равны "+ "`" +firstNumber+ "`");
-        
+        if (a > b && a > c)
+        {
+          return(a);
+        }
+        else if (b > a && b > c) 
+        {
+           return(b);
+        }
+        else 
+        {
+            return(c);
+        }
     }
 
     // Не удаляйте и не меняйте метод Main!
     static public void Main(string[] args)
     {
-        int firstNumber,
-            secondNumber;
+        int a,
+            b,
+            c;
 
-        if (args.Length >= 2)
+        if (args.Length >= 3)
         {
-            firstNumber = int.Parse(args[0]);
-            secondNumber = int.Parse(args[1]);
+            a = int.Parse(args[0]);
+            b = int.Parse(args[1]);
+            c = int.Parse(args[2]);
         }
         else
         {
             // Здесь вы можете поменять значения для отправки кода на Выполнение
-            firstNumber = 9;
-            secondNumber = 9;
+            a = 10;
+            b = 6;
+            c = 7;
         }
 
         // Не удаляйте строки ниже
-        CompareNumbers(firstNumber, secondNumber);
+        int result = FindMax(a, b, c);
+        System.Console.WriteLine($"{result}");
     }
 }
